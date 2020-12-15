9bfe66d24ae4ea3df40d4810826f7442f2edeb84
Ricardo Augusto Vicentini <ric_vicentini@hotmail.com>
2020-12-15 07:33:13 -0300

    mario 5

---
+++ 2020/lecutre 8/LUA/Mario/Mario 5/Player.lua
@@ -0,0 +1,25 @@
+Player = Class{}
+
+local MOVE_SPEED = 80
+function Player:init(map)
+  self.width = 16
+  self.height = 20
+
+  self.x = map.tileWidth * 10
+  self.y = map.tileHeight * (map.mapHeight / 2 - 1) - self.height
+
+  self.texture = love.graphics.newImage('graphics/blue_alien.png')
+  self.frames = generateQuads(self.texture, self.width, self.height)
+end
+
+function Player:update(dt)
+  if love.keyboard.isDown('a') then
+    self.x = self.x - MOVE_SPEED * dt
+  elseif love.keyboard.isDown('d') then
+    self.x = self.x + MOVE_SPEED * dt
+  end
+end
+
+function Player:render()
+  love.graphics.draw(self.texture, self.frames[1], self.x, self.y)
+end
